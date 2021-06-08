0cc90ba38c9e42574d1e8154993b72e4f03facee
Alain S. Vieira <alain.vieira@institutoitn.com.br>
2021-06-08 18:24:29 -0400

    upgrade

--- src/main/java/br/com/asv/client/dto/parse/AMaskPid.java
+++ src/main/java/br/com/asv/client/dto/parse/AMaskPid.java
@@ -2,13 +2,12 @@ package br.com.asv.client.dto.parse;
 
 import java.io.Serializable;
 
-import at.favre.lib.idmask.IdMask;
 import br.com.asv.base.model.parse.IMaskPid;
 
 
-public abstract class AMaskPid<I extends Serializable,O extends Serializable> implements IMaskPid<I, O>{
+public abstract class AMaskPid<M,I extends Serializable, O extends Serializable> implements IMaskPid<I, O>{
 
 	public abstract byte[] getIdMaskKey();
 	
-	public abstract IdMask<I> getIdMask();
+	public abstract M getIdMask();
 }
